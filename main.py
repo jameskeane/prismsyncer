1294900c410674a1e36ef94a55f04e3ee8b42fc7
James Keane <james.keane.github@gmail.com>
2023-12-05 02:34:00 -0500

    Fix pep8 errors.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from syncthing import Syncthing, SyncthingError
-import os, io
-import dotenv; dotenv.load_dotenv()
-from urllib.parse import urlparse, unquote
-from webdav3.client import Client as WebDAVClient
 from webdav3.exceptions import RemoteResourceNotFound
-
+from webdav3.client import Client as WebDAVClient
+from urllib.parse import urlparse, unquote
+from syncthing import Syncthing, SyncthingError
+import os
+import io
+import dotenv
+dotenv.load_dotenv()
 
 
 class Syncer:
@@ -29,15 +30,17 @@ class Syncer:
         res.read_from(str(last_id))
 
     def handle_file_synced(self, folder, filename):
-        if not folder in self.mappings: return
+        if not folder in self.mappings:
+            return
         print(f"Uploading {filename} from folder {folder}")
-        
+
         fullpath = os.path.join(self.mappings[folder], filename)
         if not os.path.isfile(fullpath):
             print("You likely have a configuration issue, syncthing is reporting a sync -- but we dont see the file mapped")
             return False
 
-        self.webdav.upload_sync(remote_path=f"import/{filename}", local_path=fullpath)
+        self.webdav.upload_sync(
+            remote_path=f"import/{filename}", local_path=fullpath)
         return True
 
     def run(self):
@@ -46,7 +49,8 @@ class Syncer:
         print(f"Starting from the last syncthing event id {last_id}")
 
         # subscribe to syncthing's event stream
-        event_stream = self.syncthing.events(filters=("ItemFinished",), last_seen_id=last_id)
+        event_stream = self.syncthing.events(
+            filters=("ItemFinished",), last_seen_id=last_id)
         for event in event_stream:
             ed = event['data']
             if ed['type'] == 'file' and ed['action'] == 'update':
@@ -54,9 +58,9 @@ class Syncer:
                     self.write_last_syncid(event['id'])
 
 
-
 def _mappings():
-    return dict( x.split(':') for x in os.getenv('MAPPINGS').split(',') )
+    return dict(x.split(':') for x in os.getenv('MAPPINGS').split(','))
+
 
 def _syncthing():
     KEY = os.getenv('SYNCTHING_API_KEY')
@@ -64,6 +68,7 @@ def _syncthing():
     HOST, PORT, IS_HTTPS = url.hostname, url.port, url.scheme == 'https'
     SSL_CERT_FILE = os.getenv('SYNCTHING_CERT_FILE')
     return Syncthing(KEY, HOST, PORT, 100.0, IS_HTTPS, SSL_CERT_FILE)
+
 
 def _webdav():
     url = os.getenv('PHOTOPRISM_URL')
@@ -76,13 +81,15 @@ def _webdav():
         'disable_check': True
     })
 
+
 def main():
     m = _mappings()
     s = _syncthing()
     w = _webdav()
 
-    syncer = Syncer(m, s,w)
+    syncer = Syncer(m, s, w)
     syncer.run()
+
 
 if __name__ == "__main__":
     main()
